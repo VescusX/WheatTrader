22e55e3ce5ce3c8ceb1e84bdc3753cc8f936cb83
Ewan Simms <ewancsimms@gmail.com>
2024-02-22 21:04:14 -0500

    initiate react app

--- flask-app/utils.py
+++ flask-app/utils.py
@@ -1,19 +1,15 @@
-from collections import namedtuple
 from datetime import datetime
 from sklearn.linear_model import LinearRegression, LogisticRegression
 
-import json
 import pandas as pd
 import requests
 
 
 url = 'http://eventregistry.org/api/v1/article/getArticles'
 
-with open('newsapikey.txt') as keyfile:
-    api_key = keyfile.read()
 
 def retrieve_data():
-    with open('newsapikey.txt') as keyfile:
+    with open('../newsapikey.txt') as keyfile:
         api_key = keyfile.read()
 
     start_date = '2024-01-01'
@@ -48,23 +44,29 @@ def retrieve_data():
     results = response.json()
     print("Retrieved ", results['articles']['totalResults'], " results")
 
-
+    #Unpack articles into a dataframe
     with open("sentiments_articles.pkl", "wb") as outfile:
-        articles = pd.DataFrame({ 'day': datetime.strptime(art['date'], '%Y-%m-%d'), 
+        new_articles = pd.DataFrame({ 'day': datetime.strptime(art['date'], '%Y-%m-%d'), 
                                  'sentiment': art['sentiment'],
                                  'link' : art['url']}
                                  for art in results['articles']['results'])
-        articles.to_pickle("sentiments_articles.pkl")
+        
+        old_articles = pd.read_pickle('../data/sentiments_articles.pkl')
+        articles = pd.concat([new_articles,old_articles]).drop_duplicates().reset_index(drop=True)
+        print("Model has ", articles.shape[0], " articles total")
+        articles.to_pickle('../data/sentiments_articles.pkl')
 
-def prepare_data():
+def prepare_sentiment_data():
     #Get the sentiment of the articles
-    articles = pd.read_pickle("sentiments_articles.pkl").drop(['link'],axis=1)
+    articles = pd.read_pickle('../data/sentiments_articles.pkl').drop(['link'],axis=1)
 
     #Find the average sentiment per day
     sentiments = articles.groupby('day').mean().reset_index()
-    
+    return sentiments
+
+def prepate_price_data(sentiments):
     #Get Wheat Prices
-    with open('data/HistoricalWheatPrices.csv') as csvfile:
+    with open('../data/HistoricalWheatPrices.csv') as csvfile:
         wp = pd.read_csv(csvfile)
         wp['Date'] = pd.to_datetime(wp['Date'])
        
@@ -99,21 +101,43 @@ def train_model(df):
 
     linreg = LinearRegression()
     linreg.fit(X, yi)
-    logreg = LogisticRegression(random_state=29) # Set random_state for reproducibility
-    logreg.fit(X, yo)
+    # logreg = LogisticRegression(random_state=29) # Set random_state for reproducibility
+    # logreg.fit(X, yo)
 
-    sd = (0,0,.074510,0,0,-0.388235,-0.100654)
-
-    tyer = pd.DataFrame(data = {f'day_m{i+1}': [s] for i, s in enumerate(sd) })
+    # sd = (0,0,.074510,0,0,-0.388235,-0.100654)
+    # tyer = pd.DataFrame(data = {f'day_m{i+1}': [s] for i, s in enumerate(sd) })
     # print(tyer)
+
     for n in range(X.shape[0]):
         m = X.iloc[n:n+1]
         # print(m)
-        # print(yi.iloc[n])
-        # print('\t',linreg.predict(m))
+        
+        y = yi.iloc[n]
+        pred = linreg.predict(m)
+        # print(y)
+        # print('\t',pred)
+        # print('\t',pred > 0 and y > 0 or pred < 0 and y < 0)
         # print('\t',logreg.predict(m))
+    return linreg
 
-    #print(linreg.print)
+def get_prediction(reg_model, sentiments, day):
+    sentiments[sentiments.day >= day - pd.Timedelta(days=7)]
+    pred_x = pd.DataFrame(data={'day':[day]})
+    for x in range(1,8):
+        nc = f'day_m{x}'
+        pred_x[nc] = 0.0
+        res = sentiments[sentiments['day'] == day - pd.Timedelta(days=x)]
+        if not res.empty:
+            pred_x.loc[0, nc] = res.iloc[0, res.columns.get_loc('sentiment')]
+    pred_x = pred_x.drop(['day'], axis=1)
+    prediction = reg_model.predict(pred_x)
+    return(prediction)
+
+    
         
 # retrieve_data()
-train_model(prepare_data())
+#train_model(prepate_price_data(prepare_sentiment_data()))
+    
+sents = prepare_sentiment_data()
+tm = train_model(prepate_price_data(sents))
+get_prediction(tm, sents, datetime(2023,2,22))
---
+++ react-app/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src='https://images.unsplash.com/photo-1529511582893-2d7e684dd128?q=80&w=1633&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' className="App-logo" alt="logo" /> */}
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
